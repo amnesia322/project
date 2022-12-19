0fe4378e8006eda5cd59390694ea0083664ff606
amnesia322 <dev.amnesia322@gmail.com>
2022-12-19 16:42:40 +0300

    add store

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+
+import { combineReducers, legacy_createStore } from 'redux'
+import {loadingReducer} from "./appReducer";
+
+
+const reducers = combineReducers({
+ loading: loadingReducer
+})
+
+const store = legacy_createStore(reducers)
+
+export default store
+
+export type AppStoreType = ReturnType<typeof reducers>
+
+// @ts-ignore
+window.store = store // for dev // для того чтобы автотесты видели состояние данных
