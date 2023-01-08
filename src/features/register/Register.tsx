8c94d0bf0236a3aea27e2f097828eaaff3806e04
amnesia322 <dev.amnesia322@gmail.com>
2023-01-08 14:56:17 +0300

    feat(style(added Register.tsx and Register.module.css))

--- src/features/register/Register.tsx
+++ src/features/register/Register.tsx
@@ -1,7 +1,152 @@
 import React from 'react'
 
-const Register = () => {
-  return <div>Register</div>
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import {
+  Button,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+} from '@mui/material'
+import { useFormik } from 'formik'
+
+import { PATH } from '../../app/Routes/Pages'
+
+import s from './Register.module.css'
+
+export const Register = () => {
+  const [showPassword, setShowPassword] = React.useState(false)
+
+  const handleClickShowPassword = () => setShowPassword(show => !show)
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validate: values => {
+      const errors: FormikErrorType = {}
+
+      if (!values.email) {
+        errors.email = 'Required'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+      }
+      if (!values.password) {
+        errors.password = 'Password required'
+      } else if (values.password.length < 3) {
+        errors.password = 'Password must contain at least 3 characters'
+      }
+
+      return errors
+    },
+    onSubmit: values => {
+      //dispatch(loginTC(values))
+      alert(JSON.stringify(values))
+      formik.resetForm()
+    },
+  })
+
+  return (
+    <div className={s.registerPage}>
+      <div className={s.registerContainer}>
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl>
+            <FormGroup>
+              <h2 className={s.registerTitle}>Sign Up</h2>
+              <FormControl margin="normal" fullWidth={true}>
+                <InputLabel htmlFor="register-email">Email</InputLabel>
+                <Input id="register-email" {...formik.getFieldProps('email')} />
+                {formik.touched.email && formik.errors.email ? (
+                  <div style={{ color: 'red' }}>{formik.errors.email} </div>
+                ) : null}
+              </FormControl>
+              <FormControl margin="normal" fullWidth={true}>
+                <InputLabel htmlFor="register-password">Password</InputLabel>
+                <Input
+                  id="register-password"
+                  type={showPassword ? 'text' : 'password'}
+                  {...formik.getFieldProps('password')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+                {formik.touched.password && formik.errors.password ? (
+                  <div style={{ color: 'red' }}>{formik.errors.password} </div>
+                ) : null}
+              </FormControl>
+              <FormControl margin="normal" fullWidth={true}>
+                <InputLabel htmlFor="register-password-confirm">Password</InputLabel>
+                <Input
+                  id="register-password-confirm"
+                  type={showPassword ? 'text' : 'password'}
+                  {...formik.getFieldProps('confirmPassword')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+                {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+                  <div style={{ color: 'red' }}>{formik.errors.confirmPassword} </div>
+                ) : null}
+              </FormControl>
+              <Button
+                type={'submit'}
+                variant={'contained'}
+                color={'primary'}
+                sx={{
+                  marginTop: '60px',
+                  borderRadius: '30px',
+                  fontFamily: 'Montserrat',
+                  fontStyle: 'normal',
+                  fontWeight: 500,
+                  fontSize: '16px',
+                  lineHeight: '20px',
+                  textTransform: 'none',
+                }}
+              >
+                Sign Up
+              </Button>
+            </FormGroup>
+            <FormLabel>
+              <p className={s.registerDescription}>Already have an account?</p>
+              <a href={PATH.LOGIN} target={'_blank'} rel="noreferrer" className={s.registerLink}>
+                Sign In
+              </a>
+            </FormLabel>
+          </FormControl>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default Register
+type FormikErrorType = {
+  email?: string
+  password?: string
+  confirmPassword?: string
+}
