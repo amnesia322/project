08a7211489d31da544c71ff290b2adbf38a70932
Valeriy Litvinov <valitvinoff@mail.ru>
2023-01-09 15:32:35 +0300

    refactor: profile-api, Profile.tsx

--- src/features/Profile/profileReducer.ts
+++ src/features/Profile/profileReducer.ts
@@ -1,31 +1,24 @@
 import { ThunkAction } from 'redux-thunk'
 
 import { AppStoreType } from '../../app/store'
-import {
-  LoginDataType,
-  profileAPI,
-  ProfileDataType,
-  UpdateProfileModelType,
-} from '../Profile/profileApi'
+
+import { LoginDataType, profileAPI, ProfileDataType, UpdateProfileModelType } from './profileApi'
 
 const initialState = {
-  // name: '' as string,
-  // avatar: '' as string,
-  // email: '' as string,
-  _id: '' as string,
-  created: '' as string,
-  email: '' as string,
-  name: 'UserName' as string,
-  publicCardPacksCount: 0 as number,
-  avatar: '' as string,
+  user: {
+    _id: '' as string,
+    created: '' as string,
+    email: '' as string,
+    name: 'UserName' as string,
+    publicCardPacksCount: 0 as number,
+    avatar: '' as string,
+  } as ProfileDataType,
 }
 
 type InitialStateType = typeof initialState
-type ChangeProfileNameType = ReturnType<typeof changeProfileName>
-type GetProfileEmail = ReturnType<typeof getProfileEmail>
-type GetProfileData = ReturnType<typeof getProfileName>
+type SetProfileDataType = ReturnType<typeof setProfileData>
 
-export type ProfileActionType = ChangeProfileNameType | GetProfileEmail | GetProfileData
+export type ProfileActionType = SetProfileDataType
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
@@ -39,50 +32,45 @@ export const profileReducer = (
   action: ProfileActionType
 ): InitialStateType => {
   switch (action.type) {
-    case 'profile/CHANGE_NAME':
-      return { ...state, name: action.name }
-    case 'profile/GET_EMAIL':
-      return { ...state, email: action.email }
-    case 'profile/GET_NAME':
-      return { ...state, name: action.name }
+    case 'profile/SET_PROFILE_DATA':
+      return { ...state, user: action.user }
     default:
       return state
   }
 }
 
-export const changeProfileName = (name: string) => ({ type: 'profile/CHANGE_NAME', name } as const)
-export const getProfileEmail = (email: string) => ({ type: 'profile/GET_EMAIL', email } as const)
-export const getProfileName = (name: string) => ({ type: 'profile/GET_NAME', name } as const)
+export const setProfileData = (user: ProfileDataType) =>
+  ({ type: 'profile/SET_PROFILE_DATA', user } as const)
 
-export const changeProfileNameTC =
-  (model: UpdateProfileModelType): AppThunk =>
-  async dispatch => {
-    try {
-      const response = await profileAPI.updateProfileData(model)
-
-      dispatch(changeProfileName(model.name))
-    } catch (error) {
-      console.log('Error')
-    }
-  }
-
-export const getProfileEmailTC = (): AppThunk => async dispatch => {
+export const getProfileDataTC = (): AppThunk => async dispatch => {
   try {
     const response = await profileAPI.getProfileData()
 
-    dispatch(getProfileEmail(response.data.email))
+    dispatch(setProfileData(response.data))
   } catch (error) {
     console.log('Error')
   }
 }
 
-export const getProfileNameTC = (): AppThunk => async dispatch => {
-  try {
-    const response = await profileAPI.getProfileData()
+export const updateProfileDataTC =
+  (model: UpdateProfileModelType): AppThunk =>
+  async dispatch => {
+    try {
+      const response = await profileAPI.updateProfileData(model)
 
-    dispatch(getProfileName(response.data.name))
+      dispatch(setProfileData(response.data.updatedUser))
+    } catch (error) {
+      console.log('Error')
+    }
+  }
+
+export const logoutTC = (): AppThunk => async dispatch => {
+  try {
+    const response = await profileAPI.logOut()
+
+    dispatch(setProfileData(initialState.user))
   } catch (error) {
-    console.log('Error')
+    console.log(error)
   }
 }
 
@@ -92,16 +80,8 @@ export const loginTC =
     try {
       const response = await profileAPI.login(data)
 
-      dispatch(getProfileEmail(response.data.email))
+      // dispatch(getProfileEmail(response.data.email))
     } catch (error) {
       console.log(error)
     }
   }
-
-export const logoutTC = (): AppThunk => async dispatch => {
-  try {
-    const response = await profileAPI.logOut()
-  } catch (error) {
-    console.log(error)
-  }
-}
