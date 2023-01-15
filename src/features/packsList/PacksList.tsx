8571bb625f7545330f798f3b74013038a545c693
George Erokhin <Georgii070684@gmail.com>
2023-01-15 23:18:05 +0300

    feat(packsList) add packsList into table

--- src/features/packsList/PacksList.tsx
+++ src/features/packsList/PacksList.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 import { useAppDispatch } from '../../app/store'
 
+import { PackItem } from './packItem/PackItem'
 import { getPacksTC } from './packs-reducer'
 
 export const PacksList = () => {
   const dispatch = useAppDispatch()
-
   const onClickHandler = () => {
     dispatch(getPacksTC())
   }
 
-  return <div onClick={onClickHandler}>Hello</div>
+  return (
+    <div>
+      <div onClick={onClickHandler}>Hello Packs</div>
+      <PackItem />
+    </div>
+  )
 }
---
+++ src/features/packsList/packItem/PackItem.tsx
@@ -0,0 +1,99 @@
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+
+import { useAppSelector } from '../../../app/store'
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}))
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}))
+
+const findSubstr = (str: string) => {
+  const index = str.indexOf('T')
+
+  return str.slice(0, index)
+}
+
+export const PackItem = () => {
+  const users = useAppSelector(state => state.allCardPacks.cardPacks)
+  const createData = (
+    name: string,
+    cardsCount: number,
+    lastUpdate: string,
+    createBy: string,
+    // actions: number
+    id: string
+  ) => {
+    return { name, cardsCount, lastUpdate, createBy, id }
+  }
+
+  const rows = users.map(item =>
+    createData(
+      item.name,
+      item.cardsCount,
+      findSubstr(item.updated),
+      findSubstr(item.created),
+      item._id
+    )
+  )
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        {users.length > 0 ? (
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Name</StyledTableCell>
+              <StyledTableCell align="right">Cards</StyledTableCell>
+              <StyledTableCell align="right">Last Update</StyledTableCell>
+              <StyledTableCell align="right">Create by</StyledTableCell>
+              <StyledTableCell align="right">Actions</StyledTableCell>
+            </TableRow>
+          </TableHead>
+        ) : (
+          <TableBody>
+            <TableRow>
+              <StyledTableCell>
+                This pack is empty. Click add new card to fill this pack{' '}
+              </StyledTableCell>
+            </TableRow>
+          </TableBody>
+        )}
+        <TableBody>
+          {rows.map((row, index) => (
+            <StyledTableRow key={index}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.cardsCount}</StyledTableCell>
+              <StyledTableCell align="right">{row.lastUpdate}</StyledTableCell>
+              <StyledTableCell align="right">{row.createBy}</StyledTableCell>
+              <StyledTableCell align="right">{row.createBy}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
